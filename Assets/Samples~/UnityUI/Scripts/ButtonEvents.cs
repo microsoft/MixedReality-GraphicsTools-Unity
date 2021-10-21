f38ca69467b70d2dfa8df970e6ca153d1df9a8ec
Cameron Micka <thmicka@microsoft.com>
2021-10-21 14:05:19 -0700

    UnityUI example cleanip.

--- Assets/Samples~/UnityUI/Scripts/ButtonEvents.cs
+++ Assets/Samples~/UnityUI/Scripts/ButtonEvents.cs
@@ -5,27 +5,30 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
-public class ButtonEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+namespace Microsoft.MixedReality.GraphicsTools
 {
-    [Tooltip("Event triggered by the IPointerEnterHandler interface.")]
-    public UnityEvent OnPointerEnterEvent = new UnityEvent();
-
-    [Tooltip("Event triggered by the IPointerExitHandler interface.")]
-    public UnityEvent OnPointerExitEvent = new UnityEvent();
-
-    /// <summary>
-    /// Use to detect when the mouse (or pointer) begins to hover over a certain GameObject. 
-    /// </summary>
-    public void OnPointerEnter(PointerEventData eventData)
+    public class ButtonEvents : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
     {
-        OnPointerEnterEvent.Invoke();
-    }
+        [Tooltip("Event triggered by the IPointerEnterHandler interface.")]
+        public UnityEvent OnPointerEnterEvent = new UnityEvent();
 
-    /// <summary>
-    /// Use to detect when the mouse (or pointer) stops hovering over a certain GameObject. 
-    /// </summary>
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        OnPointerExitEvent.Invoke();
+        [Tooltip("Event triggered by the IPointerExitHandler interface.")]
+        public UnityEvent OnPointerExitEvent = new UnityEvent();
+
+        /// <summary>
+        /// Use to detect when the mouse (or pointer) begins to hover over a certain GameObject. 
+        /// </summary>
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            OnPointerEnterEvent.Invoke();
+        }
+
+        /// <summary>
+        /// Use to detect when the mouse (or pointer) stops hovering over a certain GameObject. 
+        /// </summary>
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            OnPointerExitEvent.Invoke();
+        }
     }
 }
