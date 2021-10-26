c8e6f61343e77cf051d048a0a95cfbb7eda79f16
Pink Bobsledder 🌮 <pinkb@microsoft.com>
2021-10-26 10:50:52 -0700

    fixed file name to match class name

---
+++ Assets/Editor/CanvasElementMeshInspector.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.﻿
+
+using UnityEditor;
+
+namespace Microsoft.MixedReality.GraphicsTools.Editor
+{
+    [CustomEditor(typeof(CanvasElementMesh))]
+    public class CanvasElementMeshInspector : UnityEditor.Editor
+    {
+        [MenuItem("GameObject/UI/Mesh - Graphics Tools")]
+        private static void CreateCanvasElementMesh(MenuCommand menuCommand)
+        {
+            InspectorUtilities.CreateGameObjectFromMenu<CanvasElementMesh>(menuCommand);
+        }
+    }
+}
