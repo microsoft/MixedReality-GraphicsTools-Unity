f3d06a950cd48e9e848767e58c2e2cc39adf7b69
Cameron Micka <thmicka@microsoft.com>
2022-06-10 16:18:17 -0700

    Revert changes.

--- com.microsoft.mrtk.graphicstools.unity/Runtime/Experimental/Acrylic/Scripts/AcrylicFilterDual.cs
+++ com.microsoft.mrtk.graphicstools.unity/Runtime/Experimental/Acrylic/Scripts/AcrylicFilterDual.cs
@@ -32,6 +32,11 @@ namespace Microsoft.MixedReality.GraphicsTools
             buffers = new List<RenderTexture>();
         }
 
+        ~AcrylicFilterDual()
+        {
+            FreeBuffers();
+        }
+
         public void QueueBlur(CommandBuffer cmd, RenderTexture image, int iterations)
         {
             if (image.width != lastWidth || image.height != lastHeight || iterations != lastIterations)
@@ -74,14 +79,6 @@ namespace Microsoft.MixedReality.GraphicsTools
             Graphics.ExecuteCommandBuffer(cmd);
             Profiler.EndSample();
         }
-        public void FreeBuffers()
-        {
-            for (int i = 0; i < buffers.Count; i++)
-            {
-                Object.Destroy(buffers[i]);
-            }
-            buffers.Clear();
-        }
 
         private void InitBuffers(int width, int height, int iterations)
         {
@@ -93,6 +90,15 @@ namespace Microsoft.MixedReality.GraphicsTools
                 nextWidth = (nextWidth + 1) / 2;
                 nextHeight = (nextHeight + 1) / 2;
             }
+        }
+
+        private void FreeBuffers()
+        {
+            for (int i = 0; i < buffers.Count; i++)
+            {
+                Object.Destroy(buffers[i]);
+            }
+            buffers.Clear();
         }
 
         private void LocalBlit(CommandBuffer cmd, RenderTexture source, RenderTexture target, Material material, int pass)
