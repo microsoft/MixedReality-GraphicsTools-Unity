7b7aed3e2ba1907d5251d52154cf1b5744334f86
Brandon Palmer <brandonpalmerdesigns@gmail.com>
2022-12-05 21:42:18 -0800

    Matryoshka Anim

--- GraphicsToolsUnityProject/Assets/MyScripts/Rotate.cs
+++ GraphicsToolsUnityProject/Assets/MyScripts/Rotate.cs
@@ -6,13 +6,7 @@ public class Rotate : MonoBehaviour
 {
     public float spin = .5f;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+   
     void Update()
     {
         this.gameObject.transform.Rotate(0,spin,0);
