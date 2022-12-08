80dcfcbb71baeb13ce0a94a63d8533b180285506
Brandon Palmer <brandonpalmerdesigns@gmail.com>
2022-12-07 17:17:13 -0800

    Changed File Structure
    Added Clipping Primitives Directory

--- com.microsoft.mrtk.graphicstools.unity/Samples/ClippingPrimitives/Scripts/Spin.cs
+++ com.microsoft.mrtk.graphicstools.unity/Samples/ClippingPrimitives/Scripts/Spin.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotate : MonoBehaviour
+public class Spin : MonoBehaviour
 {
     public float spin = .5f;
 
