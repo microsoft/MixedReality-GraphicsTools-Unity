ffcba0c9b80a5505aae6d5ae0b9095fb9fe88f23
Lars Simkins <lars.simkins@gmail.com>
2023-06-08 09:46:25 -0700

    Cleaning up usings & comments

--- com.microsoft.mrtk.graphicstools.unity/Editor/Inspectors/MeshOutlineHierarchyInspector.cs
+++ com.microsoft.mrtk.graphicstools.unity/Editor/Inspectors/MeshOutlineHierarchyInspector.cs
@@ -1,7 +1,7 @@
-using System.Collections;
-using System.Collections.Generic;
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
 using UnityEditor;
-using UnityEngine;
 
 namespace Microsoft.MixedReality.GraphicsTools.Editor
 {
