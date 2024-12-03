1478dc86a3b5d776b6ec23895f54eb8854bd7c8e
Cameron Micka <cameron.micka@microsoft.com>
2024-12-02 16:38:40 -0800

    WIP light combine.

--- com.microsoft.mrtk.graphicstools.unity/Editor/LightCombiner/LightCombinerWindow.cs
+++ com.microsoft.mrtk.graphicstools.unity/Editor/LightCombiner/LightCombinerWindow.cs
@@ -1,13 +1,18 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.IO;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Microsoft.MixedReality.GraphicsTools.Editor
 {
 	public class LightCombinerWindow : EditorWindow
 	{
+		private string errorText = null;
+
 		[MenuItem("Window/Graphics Tools/Light Combiner")]
 		private static void ShowWindow()
 		{
@@ -25,13 +30,59 @@ namespace Microsoft.MixedReality.GraphicsTools.Editor
 				// TODO
 			}
 			EditorGUILayout.EndVertical();
+
+			EditorGUILayout.Space();
+
+			EditorGUILayout.BeginVertical("Box");
+			{
+				EditorGUILayout.Space();
+
+				if (GUILayout.Button("Combine Lighting"))
+				{
+					Save();
+				}
+
+				EditorGUILayout.Space();
+
+				if (!string.IsNullOrEmpty(errorText))
+				{
+					EditorGUILayout.HelpBox(errorText, MessageType.Error);
+				}
+				else
+				{
+					EditorGUILayout.HelpBox("TODO", MessageType.Info);
+				}
+			}
+			EditorGUILayout.EndVertical();
 		}
 
-		private void Save(MeshUtility.MeshCombineResult result)
+		private void Save()
 		{
 			var watch = System.Diagnostics.Stopwatch.StartNew();
 
-			// TODO
+			// Get the active scene
+			var currentScene = SceneManager.GetActiveScene();
+			var currentScenePath = currentScene.path;
+
+			if (string.IsNullOrEmpty(currentScenePath))
+			{
+				errorText = "Current scene is not saved. Please save the scene first.";
+				return;
+			}
+
+			// Generate a new path for the duplicated scene
+			var newScenePath = Path.Combine(Path.GetDirectoryName(currentScenePath), Path.GetFileNameWithoutExtension(currentScenePath) + "_Copy.unity");
+
+			// Copy the scene file
+			if (AssetDatabase.CopyAsset(currentScenePath, newScenePath))
+			{
+				// Open the duplicated scene
+				EditorSceneManager.OpenScene(newScenePath);
+			}
+			else
+			{
+				errorText = "Failed to duplicate the scene.";
+			}
 
 			Debug.LogFormat("LightCombinerWindow.Save took {0} ms.", watch.ElapsedMilliseconds);
 		}
