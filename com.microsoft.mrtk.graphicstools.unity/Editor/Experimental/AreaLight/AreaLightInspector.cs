9b497c9c536c5e48d97278bdaf61a10a2a843f47
Cameron Micka <thmicka@microsoft.com>
2025-03-26 12:05:54 -0700

    Better handles.

--- com.microsoft.mrtk.graphicstools.unity/Editor/Experimental/AreaLight/AreaLightInspector.cs
+++ com.microsoft.mrtk.graphicstools.unity/Editor/Experimental/AreaLight/AreaLightInspector.cs
@@ -30,9 +30,18 @@ namespace Microsoft.MixedReality.GraphicsTools.Editor
 				Handles.color = Color.gray;
 			}
 
-			using (new Handles.DrawingScope(light.transform.localToWorldMatrix))
+			EditorGUI.BeginChangeCheck();
+			Vector2 size = DrawRectHandles(light.transform.rotation, light.transform.position, light.Size);
+			if (EditorGUI.EndChangeCheck())
 			{
-				Handles.DrawWireCube(Vector3.zero, light.Size);
+				Undo.RecordObject(light, "Adjust Area Light Size");
+				light.Size = size;
+			}
+
+			// Draw the area light's normal only if it will not overlap with the current tool.
+			if (!((Tools.current == Tool.Move || Tools.current == Tool.Scale) && Tools.pivotRotation == PivotRotation.Local))
+			{
+				Handles.DrawLine(light.transform.position, light.transform.position + light.transform.forward);
 			}
 
 			Handles.color = new Color(255.0f / 255.0f, 165.0f / 255.0f, 0.0f / 255.0f); // Orange.
@@ -71,11 +80,71 @@ namespace Microsoft.MixedReality.GraphicsTools.Editor
 
 			if (gameObject != null)
 			{
-				gameObject.transform.position = new Vector3(0.0f, 3.0f, 0.0f);
+				gameObject.transform.position = new Vector3(0.0f, 1.0f, 0.0f);
 				Quaternion rotation = new Quaternion();
-				rotation.eulerAngles = new Vector3(50.0f, -30.0f, 0.0f);
+				rotation.eulerAngles = new Vector3(0.0f, 180.0f, 0.0f);
 				gameObject.transform.rotation = rotation;
 			}
+		}
+
+		private static float SizeSlider(Vector3 p, Vector3 d, float r)
+		{
+			Vector3 position = p + d * r;
+			float size = HandleUtility.GetHandleSize(position);
+			bool temp = GUI.changed;
+			GUI.changed = false;
+			position = Handles.Slider(position, d, size * 0.03f, Handles.DotHandleCap, 0.0f);
+
+			if (GUI.changed)
+			{
+				r = Vector3.Dot(position - p, d);
+			}
+
+			GUI.changed |= temp;
+			return r;
+		}
+		private static Color ToActiveColorSpace(Color color)
+		{
+			return (QualitySettings.activeColorSpace == ColorSpace.Linear) ? color.linear : color;
+		}
+
+		private static Vector2 DrawRectHandles(Quaternion rotation, Vector3 position, Vector2 size)
+		{
+			Vector3 up = rotation * Vector3.up;
+			Vector3 right = rotation * Vector3.right;
+
+			float halfWidth = 0.5f * size.x;
+			float halfHeight = 0.5f * size.y;
+
+			Vector3 topRight = position + up * halfHeight + right * halfWidth;
+			Vector3 bottomRight = position - up * halfHeight + right * halfWidth;
+			Vector3 bottomLeft = position - up * halfHeight - right * halfWidth;
+			Vector3 topLeft = position + up * halfHeight - right * halfWidth;
+
+			// Draw the rectangle.
+			Handles.DrawLine(topRight, bottomRight);
+			Handles.DrawLine(bottomRight, bottomLeft);
+			Handles.DrawLine(bottomLeft, topLeft);
+			Handles.DrawLine(topLeft, topRight);
+
+			// Give handles twice the alpha of the lines.
+			Color originalColor = Handles.color;
+			Color color = Handles.color;
+			color.a = Mathf.Clamp01(Handles.color.a * 2);
+			Handles.color = ToActiveColorSpace(color);
+
+			// Draw the handles.
+			halfHeight = SizeSlider(position, up, halfHeight);
+			halfHeight = SizeSlider(position, -up, halfHeight);
+			halfWidth = SizeSlider(position, right, halfWidth);
+			halfWidth = SizeSlider(position, -right, halfWidth);
+
+			size.x = Mathf.Max(0.0f, 2.0f * halfWidth);
+			size.y = Mathf.Max(0.0f, 2.0f * halfHeight);
+
+			Handles.color = originalColor;
+
+			return size;
 		}
 	}
 }
